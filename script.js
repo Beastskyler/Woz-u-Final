52c18a63778a9827c6daa8c63cf2a57ae1da7c3a
Beastskyler <55608303+Beastskyler@users.noreply.github.com>
2020-05-02 15:40:01 -0700

    good

--- script.js
+++ script.js
@@ -7,6 +7,11 @@ $(document).ready(function () {
     });
 });
 
+function changeColor(bc, c){
+  document.body.style.backgroundColor = bc;
+  document.body.style.color = c;
+}
+
 let gitHubRequest = new XMLHttpRequest();
 gitHubRequest.onreadystatechange = function () {
   if (this.readyState == 4 && this.status == 200) {
